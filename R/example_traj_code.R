24457f46d8aa9a19a297b31409913cb76b480710
由比滨结衣 <fushanying@mac.mynetworksettings.com>
2026-04-17 20:00:29 -0400

    plots of forests finished

--- R/example_traj_code.R
+++ R/example_traj_code.R
@@ -1,10 +1,16 @@
 # From Lei Song
 
 library(timeseriesTrajectories)
-source("R/revised_funcs_timeseriesTrajectories.R")
+library(ggplot2)
+library(terra)
+library(here)
+library(dplyr)
+library(tidyterra)
+library(pdftools)
 
 # I just read the file that you shared with me.
-lc_stack <- rast("/Users/leisong/Downloads/peru_lc_stack_2000_2020.tif")
+lc_stack <- rast(here("processed_data", "lc_stack_2000_2020.tif"))
+
 
 ##### Use forest as an example land cover type #####
 # Make sure you do for other types. Cropland could be an interesting one.
@@ -21,8 +27,16 @@ vert_units <- "Km2"
 
 # 2. Check time series change in areas
 # Use this following code to replace dataPreview
+png(
+  filename = here("Project_Image", "forest_timeseries_change_area.png"),
+  width = 2400,
+  height = 1800,
+  res = 300
+)
+
 plot_timeseries(
   forest_area, timepoints = years, vertunits = vert_units, xAngle = 0) +
+  scale_y_continuous(limits = c(800000, 820000))+
   theme(
     panel.background = element_rect(fill = "transparent", colour = NA),
     axis.line.y = element_line(color = "black", linewidth = 0.5),
@@ -34,12 +48,19 @@ plot_timeseries(
     legend.title = element_text(size = 18, face = "bold"),
     legend.text = element_text(size = 12, face = "bold"))
 
+dev.off()
+
 
 # 3. Check the overall change conditions in the time series
 ## Summarize the presence data
 num_pres_change <- presenceData(forest_binary, nodata = -999)
 
 ## Plot it. Just use the following code, do not use presencePlot
+png(filename = here("Project_Image", "forest_number_of_presences.png"),
+  width = 2100,
+  height = 1800,
+  res = 300
+)
 ggplot() +
   geom_spatraster(
     data = num_pres_change$`Data for number of presence`) +
@@ -55,6 +76,8 @@ ggplot() +
 
 max_val <- global(num_pres_change$`Data for number of changes`,
                   "max", na.rm = TRUE)[[1]]
+dev.off()
+
 ggplot() +
   geom_spatraster(data = num_pres_change$`Data for number of changes`) +
   scale_fill_distiller(
@@ -76,13 +99,19 @@ traj_data <- rastertrajData(forest_area, zeroabsence = 'yes')
 
 # Use plot_trajectory instead of trajPlot. And you could customize everything
 # after the function.
+png(
+  filename = here("Project_Image", "forest_traj_plot.png"),
+  width = 2400,
+  height = 1800,
+  res = 300
+)
 plot_trajectory(traj_data) +
   theme_void(base_size = 12) +
   theme(
     panel.grid = element_blank(),
     legend.title = element_text(face = "bold"),
     legend.position = "right")
-
+dev.off()
 
 # 5. Stack everything
 ## Here you could just subset a few years to check.
@@ -94,7 +123,18 @@ stackbar_data <- rasterstackData(
   x = forest_yr_check, timePoints = tps)
 
 ## Plot it. Use plot_stackbar instead of stackbarPlot
-plot_stackbar(stackbar_data,
+
+pdf(file = here("Project_Image", "stackbar_plot.pdf"), width = 10, height = 6)
+pdf_convert(
+  pdf = here("Project_Image", "stackbar_plot.pdf"),
+  format = "png",
+  filenames = c(
+    here("Project_Image", "forest_stackbar_plot_1.png"),
+    here("Project_Image", "forest_stackbar_plot_2.png")
+  )
+)
+
+ plot_stackbar(stackbar_data,
              axisSize = 10,
              lbAxSize = 10,
              lgSize = 7.5,
@@ -106,3 +146,6 @@ plot_stackbar(stackbar_data,
              upperlym2 = 0.5,
              lymby2 = 0.1,
              xAngle = 0)
+
+dev.off()
+
